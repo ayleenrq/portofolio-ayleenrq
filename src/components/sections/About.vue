a02849ff9df32f98736d8c6764b18b9c8b95d601
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-07-19 21:53:33 +0700

    update about

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -10,8 +10,8 @@
   <section id="about">
     <div class="flex flex-col gap-8 items-center justify-center h-150"> 
       <h2 class="text-4xl font-bold">About Me</h2>
-      <div class="flex gap-12 items-center justify-center">
-        <p class="text-2xl text-gray-600 max-w-xl leading-10">Hi, I'm Ayleen Ruhul Qisthy, a passionate UI/UX Designer and Frontend Developer based in Indonesia. With a strong foundation in design and development, I create visually appealing and user-friendly interfaces that enhance user experience.</p>
+      <div class="flex gap-20 items-center justify-center">
+        <p class="text-2xl text-gray-600 max-w-xl leading-10">Hi, I'm Ayleen Ruhul Qisthy, a passionate UI/UX Designer and Frontend Developer Enthusiast based in Malang Indonesia. With a strong foundation in design and development, I create visually appealing and user-friendly interfaces that enhance user experience.</p>
         <img src="../../assets/ayleen.jpg" class="w-80 rounded-4xl" alt="">
       </div>
     </div>
