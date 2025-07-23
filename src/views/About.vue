33f9b451049e29756655ba42c6217740161347d2
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-07-23 20:34:40 +0700

    update page structure

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Navigation from '../components/layout/Navigation.vue'
+import About from '../components/sections/about/About.vue'
+import Education from '../components/sections/about/Education.vue'
+import Experience from '../components/sections/about/Experience.vue'
+import Skills from '../components/sections/about/Skills.vue'
+import Footer from '../components/layout/Footer.vue'
+</script>
+
+<template>
+  <Navigation />
+
+  <main class="flex flex-col gap-10">
+    <section id="about">
+      <About name="About" />
+    </section>
+
+    <section id="education">
+      <Education name="Education" />
+    </section>
+
+    <section id="experience">
+      <Experience name="Experience" />
+    </section>
+
+    <section id="skills">
+      <Skills name="Skills" />
+    </section>
+  </main>
+
+  <Footer />
+</template>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navigation from '../components/layout/Navigation.vue'
-import Hero from '../components/sections/home/Hero.vue'
+import UIUX from '../components/sections/projects/UIUX.vue'
+import WebDev from '../components/sections/projects/WebDev.vue'
 import Footer from '../components/layout/Footer.vue'
 </script>
 
@@ -8,8 +9,9 @@ import Footer from '../components/layout/Footer.vue'
   <Navigation />
 
   <main class="flex flex-col gap-10">
-    <section id="hero">
-      <Hero name="Hero" />
+    <section id="projects">
+      <UIUX name="UIUX" />
+      <WebDev name="WebDev" />
     </section>
   </main>
 
